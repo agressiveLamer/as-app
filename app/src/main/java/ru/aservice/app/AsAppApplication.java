a712a07dc3c8a8604e82b08733fc73f21fc61e1a
genrihromanov <setskeys1332@gmail.com>
2023-02-07 19:22:36 +0300

    Initial commit

---
+++ app/src/main/java/ru/aservice/app/AsAppApplication.java
@@ -0,0 +1,13 @@
+package ru.aservice.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsAppApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsAppApplication.class, args);
+	}
+
+}
---
+++ app/src/main/java/ru/aservice/app/controller/CarController.java
@@ -0,0 +1,8 @@
+package ru.aservice.app.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CarController  {
+
+}
